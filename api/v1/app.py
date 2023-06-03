e4ebb6e8177144b2b53b6a7d9893401c4bba2dda
Muideen27 <ilorimuideen0000@gmail.com>
2023-06-03 16:07:02 +0000

    app.py

---
+++ api/v1/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+module to start using api
+"""
+from flask import Flask, jsonify, make_response
+from models import storage
+from api.v1.views import app_views
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def exit(exception):
+    ''' exit api in case of unexpected error '''
+    storage.close()
+
+if __name__ == '__main__':
+    host = getenv("HBNB_API_HOST")
+    port = getenv("HBNB_API_PORT")
+    if host is None:
+        host = '0.0.0.0'
+    if port is None:
+        port = 5000
+    app.run(host=host, port=port)
